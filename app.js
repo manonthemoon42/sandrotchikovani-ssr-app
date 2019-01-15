1c7aeeefce6cfeb85885ec95d980e9312b32e819
Sandro Tchikovani <tchikovani.sandro@gmail.com>
2019-01-14 23:23:37 -0800

    Fixed other routes when directly accessed from Express server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const logger = require('morgan');
 
+var usersRouter = require('./server/users/routes/users');
+
 var app = express();
 
 // Express configuration
@@ -11,5 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api/users', usersRouter);
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 module.exports = app;
