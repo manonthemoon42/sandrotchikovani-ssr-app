675ee9b1a7a1331661c602a8d0c93526818ca618
Sandro Tchikovani <tchikovani.sandro@gmail.com>
2019-01-14 23:37:26 -0800

    Improved content of Bienvenue Page (highlight keywords, horizontal sizing etc)

--- client/src/Bienvenue/BienvenuePage.js
+++ client/src/Bienvenue/BienvenuePage.js
@@ -12,18 +12,18 @@ import './BienvenuePage.css';
 class BienvenuePage extends Component {
     render() {
         return (
-            <div id="BienvenueContent">
-                <Container>
-                    <Row>
+            <div>
+                <Container id="BienvenueContainer">
+                    <Row id="RowImage">
                         <Col>
                             <Image width="150px" height="150px" src={AvatarImg} roundedCircle/>
                         </Col>
                     </Row>
                     <Row id="RowText">
                         <Col>
+                            <p id="Hello">Bonjour!</p><br/>
                             <p>
-                                Bonjour!<br/><br/>
-                                My name is Sandro TCHIKOVANI, a French Entrepreneur and Software Engineer. You can see the different projects I’ve worked on by clicking here.<br/><br/>
+                                My name is Sandro TCHIKOVANI, a <span className="StrongWord">French Entrepreneur</span> and <span className="StrongWord">Software Engineer</span>. You can see the different projects I’ve worked on by clicking <span className="StrongWord">here</span>.<br/><br/>
                                 Passionate of technologies, I’ve always loved building things. As a child, I’ve started with LEGO constructions and today focused on Software constructions. Besides technology, I also like sports , traveling, food, movies, music, video games & more.
                             </p>
                         </Col>
